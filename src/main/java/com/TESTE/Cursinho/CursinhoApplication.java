8dc1b00bf37e9b5852028513776554dd0880a9ac
joao pedro <joao.ferreira01br@gmail.com>
2023-09-09 02:34:11 -0300

    Projeto criado

---
+++ src/main/java/com/TESTE/Cursinho/CursinhoApplication.java
@@ -0,0 +1,13 @@
+package com.TESTE.Cursinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TESTE/Cursinho/CursinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TESTE.Cursinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
