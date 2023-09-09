7b55cbf39501fa1d5802d6a647b4e9d3241b238c
joao pedro <joao.ferreira01br@gmail.com>
2023-09-09 03:07:52 -0300

    Entidade USER e USERESOURCE

---
+++ src/main/java/com/TESTE/Cursinho/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.TESTE.Cursinho.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.TESTE.Cursinho.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "maria", "maria@gmail.com", "99999999", "12345");
+	return ResponseEntity.ok().body(u);
+	}
+	
+
+}
+
