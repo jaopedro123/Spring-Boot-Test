44b17685186d995cee0222cbecf7406a8daa2758
joao pedro <joao.ferreira01br@gmail.com>
2023-09-10 21:44:18 -0300

    Category

---
+++ src/main/java/com/TESTE/Cursinho/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.TESTE.Cursinho.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.TESTE.Cursinho.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+
+}
